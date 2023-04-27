bbd9eb241e68bdad3824a5036df057556c8d0ed6
Sheridan Lugo <sheridanlugo@gmail.com>
2023-04-27 13:54:41 -0700

    three js attempts

--- 07-import-model/src/App.jsx
+++ 07-import-model/src/App.jsx
@@ -5,6 +5,8 @@ import * as THREE from 'three';
 // import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 // import { VOXLoader } from 'three/examples/jsm/loaders/VOXLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 
 import SceneInit from './lib/SceneInit';
 
@@ -14,28 +16,40 @@ function App() {
     test.initialize();
     test.animate();
 
-    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
-    // const boxMaterial = new THREE.MeshNormalMaterial();
-    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    // test.scene.add(boxMesh);
-
+    //New model 
     let loadedModel;
+
+    //load model 
+
+    
     const glftLoader = new GLTFLoader();
-    glftLoader.load('./assets/shiba/scene.gltf', (gltfScene) => {
+    glftLoader.load('./assets/earth_globe/scene.gltf', (gltfScene) => {
       loadedModel = gltfScene;
       // console.log(loadedModel);
 
-      gltfScene.scene.rotation.y = Math.PI / 8;
-      gltfScene.scene.position.y = 3;
+      //=====scene=====
+      gltfScene.scene.rotation.y = Math.PI / 6;
+      gltfScene.scene.position.y = 2;
       gltfScene.scene.scale.set(10, 10, 10);
+      gltfScene.scene.background = '0xffffff'; //change background color to white 
       test.scene.add(gltfScene.scene);
     });
 
+      //camera 
+      const camera = new THREE.PerspectiveCamera( 45, width / height, 1, 1000 );
+      scene.add( camera );
+
+
+    //====orbit disabled =====
+    controls = new THREE.OrbitControls (camera); 
+    // to disable zoom
+    controls.enableZoom = false;
+
     const animate = () => {
       if (loadedModel) {
-        loadedModel.scene.rotation.x += 0.01;
-        loadedModel.scene.rotation.y += 0.01;
-        loadedModel.scene.rotation.z += 0.01;
+        loadedModel.scene.rotation.x += 0.0;
+        loadedModel.scene.rotation.y += 0.0;
+        loadedModel.scene.rotation.z += 0.0;
       }
       requestAnimationFrame(animate);
     };
